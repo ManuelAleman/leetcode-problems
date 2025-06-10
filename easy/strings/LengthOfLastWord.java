cf6c2a2296568bfd1fce42b4ad6763a2af403dc2
ManuelAleman <manuelitoaleman230@icloud.com>
2025-06-09 19:00:28 -0600

    ADDED STRING PROBLEM

---
+++ easy/strings/LengthOfLastWord.java
@@ -0,0 +1,15 @@
+//Leetcode Problem: https://leetcode.com/problems/length-of-last-word/
+package easy.strings;
+
+public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+        String[]words = s.split(" ");
+
+        return words[words.length-1].length();
+    }
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("Hello World"));
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+        System.out.println(lengthOfLastWord("luffy is still joyboy"));
+    }
+}
