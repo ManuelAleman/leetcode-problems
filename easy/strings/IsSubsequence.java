f7cd7f60b243a7040668d9c7cafd5bede0e150a0
ManuelAleman <manuelitoaleman230@icloud.com>
2025-06-13 15:07:47 -0700

    LEETCODE 392

---
+++ easy/strings/IsSubsequence.java
@@ -0,0 +1,24 @@
+//LeetCode Problem: https://leetcode.com/problems/is-subsequence/
+package easy.strings;
+
+public class IsSubsequence {
+    
+    public static boolean isSubsequence(String s, String t) {
+        int i=0, j=0;
+        while(i< s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+
+            j++;
+
+        }
+
+        return i == s.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc"));
+        System.out.println(isSubsequence("axc", "ahbgdc"));
+    }
+}
